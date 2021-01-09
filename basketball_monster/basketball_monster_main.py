e3eb7a3737264a1a0624851171fecc492c3ffa1a
Nicholas Doglio <NicholasDoglio@Gmail.com>
2021-01-09 12:40:25 -0500

    Remove printing out path

--- basketball_monster/basketball_monster_main.py
+++ basketball_monster/basketball_monster_main.py
@@ -19,15 +19,12 @@
 #  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 #  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #  SOFTWARE.
-import os
-
 from basketball_monster.player_data_converter import convert_element_to_player_info
 from basketball_monster.scraper import BasketballMonsterWebScraper
 from sheets.google_sheets_client import GoogleSheetsClient
 
 
 def main():
-    print(os.getcwd())
     """
      Main Script for updating our Google Sheet with data from Basketball Monster.
     """
