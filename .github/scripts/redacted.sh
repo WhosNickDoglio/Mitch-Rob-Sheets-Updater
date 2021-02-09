98f8cd678dc10146f82dd3f108b8f136ac31f74d
Nicholas Doglio <NicholasDoglio@Gmail.com>
2021-02-08 21:29:32 -0500

    Make script bash

--- .github/scripts/redacted.sh
+++ .github/scripts/redacted.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 
 REPORT=`python -m redacted.redacted_covid_roster_check ${{ secrets.YAHOO_CLIENT_ID }} ${{ secrets.YAHOO_CLIENT_SECRET }} ${{ secrets.YAHOO_REFRESH_TOKEN }}`
